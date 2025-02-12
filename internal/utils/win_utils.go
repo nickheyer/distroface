de7a694e5759d9f128a1b871ad70ba689ab93274
Nicholas Heyer <nicholas.heyer@ghsiss.com>
2025-02-12 13:16:34 -0800

    impl actual disk space metric for windows instead of empty stub

--- internal/utils/win_utils.go
+++ internal/utils/win_utils.go
@@ -4,11 +4,32 @@ package utils
 
 import (
 	"github.com/nickheyer/distroface/internal/models"
+	"golang.org/x/sys/windows"
 )
 
 func GetDiskInfo(dir string) models.DiskInfo {
 	var info models.DiskInfo
-	info.DiskTotal = 0
-	info.DiskAvailable = 0
+	var (
+		directoryName              = windows.StringToUTF16Ptr(dir)
+		freeBytesAvailableToCaller uint64
+		totalNumberOfBytes         uint64
+		totalNumberOfFreeBytes     uint64
+	)
+
+	err := windows.GetDiskFreeSpaceEx(
+		directoryName,
+		&freeBytesAvailableToCaller,
+		&totalNumberOfBytes,
+		&totalNumberOfFreeBytes,
+	)
+
+	if err != nil {
+		info.DiskTotal = 0
+		info.DiskAvailable = 0
+	} else {
+		info.DiskTotal = int64(totalNumberOfBytes)
+		info.DiskAvailable = int64(totalNumberOfFreeBytes)
+	}
+
 	return info
 }
