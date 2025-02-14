dc02488bd017ba7b555bce4895be983fa7e30cab
Nicholas Heyer <nicholas.heyer@ghsiss.com>
2025-02-13 20:27:47 -0800

    docs on gh

--- web/src/routes/registry/+page.svelte
+++ web/src/routes/registry/+page.svelte
@@ -142,6 +142,7 @@
                         class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm
                                font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700
                                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                        onclick={() => window.open('https://github.com/nickheyer/distroface', '_blank')}
                     >
                         <Package class="h-4 w-4 mr-2" />
                         View Documentation
